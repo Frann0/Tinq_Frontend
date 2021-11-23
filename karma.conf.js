22ffb9ced0a6cfa0d15e4dc145df1b95ab196f8a
unknown <deengo113@gmail.com>
2021-11-23 22:47:48 +0100

    Index update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/e'),
+      dir: require('path').join(__dirname, './coverage/fatest'),
       subdir: '.',
       reporters: [
         { type: 'html' },
