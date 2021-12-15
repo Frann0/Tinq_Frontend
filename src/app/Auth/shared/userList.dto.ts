ea056b3ae402e458ead7a6b6d8403c73a9cfd504
Frann0 <50768854+Frann0@users.noreply.github.com>
2021-12-15 14:03:15 +0100

    Fixed feed and profile scaling

---
+++ src/app/Auth/shared/userList.dto.ts
@@ -0,0 +1,11 @@
+import {PermissionDto} from "./permission.dto";
+import {TokenDto} from "./token.dto";
+
+
+export interface LoggedInUserDto {
+  id: number;
+  username: string;
+  email: string;
+  permissions: PermissionDto[];
+  token: TokenDto;
+}
